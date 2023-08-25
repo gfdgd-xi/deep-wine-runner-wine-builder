866a5c19935052be506dfc6e5e9c5ab34f3da02b
gfdgd_xi <3025613752@qq.com>
2023-08-25 12:51:41 +0800

    依赖

--- build-box86-in-chroot.sh
+++ build-box86-in-chroot.sh
@@ -2,10 +2,10 @@
 cpu=$(cat /proc/cpuinfo | grep processor | wc -l)
 cd /
 apt update
-apt install sudo dpkg-dev git make python3 aria2 fakeroot cmake -y
+apt install sudo dpkg-dev git make python3 wget fakeroot cmake -y
 mkdir box
 cd box
-aria2c -x 16 -s 16 $1
+wget $1
 tar -xvf *
 cd box*/
 apt build-dep . -y
