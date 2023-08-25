8b42f0afabb199c3b18e486bb1030179818fdb8b
gfdgd_xi <3025613752@qq.com>
2023-08-25 12:27:00 +0800

    更换编译方式

--- build-box86-in-chroot.sh
+++ build-box86-in-chroot.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+cpu=$(cat /proc/cpuinfo | grep processor | wc -l)
 cd /
 apt update
-apt install sudo dpkg-dev git make aria2 fakeroot -y
+apt install sudo dpkg-dev git make aria2 fakeroot cmake -y
 mkdir box
 cd box
 aria2c -x 16 -s 16 $1
 tar -xvf *
 cd box*/
 apt build-dep . -y
-dpkg-buildpackage -b
-#cp *.deb ..
+#dpkg-buildpackage -b
+cmake .
+make -j$cpu
+cp *.deb ..
