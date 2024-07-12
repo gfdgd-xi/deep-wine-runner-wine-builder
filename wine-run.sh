fe477b1d1f3c2cb6f02fc7a06d132a9b523b0e13
gfdgd_xi <3025613752@qq.com>
2024-07-12 21:26:03 +0800

    小调

--- wine-run.sh
+++ wine-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CURRENT_DIR=$(cd $(dirname $0); pwd)
-if [[ ! -f /etc/box64.box64rc ]]; then
+if [[ ! -f ~/.box64rc ]]; then
     cp -rv $CURRENT_DIR/box64/box64.box64rc ~/.box64rc
 fi
 export LD_LIBRARY_PATH=$CURRENT_DIR/box64/x86_64-linux-gnu/:$LD_LIBRARY_PATH
