66fa8efde796d504bb5317812308d737d0db5efd
gfdgd_xi <3025613752@qq.com>
2024-07-14 15:29:30 +0800

    调整配置文件机制

--- wine-run.sh
+++ wine-run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 CURRENT_DIR=$(cd $(dirname $0); pwd)
-if [[ ! -f ~/.box64rc ]]; then
-    cp -rv $CURRENT_DIR/box64/box64.box64rc ~/.box64rc
-fi
+cp -r $CURRENT_DIR/box64/box64.box64rc ~/.box64rc
 export LD_LIBRARY_PATH=$CURRENT_DIR/box64/x86_64-linux-gnu/:$LD_LIBRARY_PATH
 $CURRENT_DIR/box64/box64 $CURRENT_DIR/bin/wine-amd64 $*
