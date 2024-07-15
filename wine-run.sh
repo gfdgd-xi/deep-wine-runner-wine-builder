956dd73f3876176a192923994d5bd58b3cae9336
gfdgd_xi <3025613752@qq.com>
2024-07-15 12:39:08 +0800

    修复无法运行其它exe的问题

--- wine-run.sh
+++ wine-run.sh
@@ -2,4 +2,4 @@
 CURRENT_DIR=$(cd $(dirname $0); pwd)
 cp -r $CURRENT_DIR/box64/box64.box64rc ~/.box64rc
 export LD_LIBRARY_PATH=$CURRENT_DIR/box64/x86_64-linux-gnu/:$LD_LIBRARY_PATH
-$CURRENT_DIR/box64/box64 $CURRENT_DIR/bin/wine-amd64 $*
+$CURRENT_DIR/box64/box64 $CURRENT_DIR/wine/bin/wine $*
