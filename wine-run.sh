de105739342daf2f4102372f5880f20e3694f133
gfdgd_xi <3025613752@qq.com>
2024-07-15 19:18:35 +0800

    新增文字提示

--- wine-run.sh
+++ wine-run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo Power by Wine Runner
+echo 由 Wine 运行器提供支持
 CURRENT_DIR=$(cd $(dirname $0); pwd)
 cp -r $CURRENT_DIR/box64/box64.box64rc ~/.box64rc
 export LD_LIBRARY_PATH=$CURRENT_DIR/box64/x86_64-linux-gnu/:$LD_LIBRARY_PATH
