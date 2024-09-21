48a6a164a9bdceac979d6d5593a17790001d7028
gfdgd_xi <3025613752@qq.com>
2024-09-21 11:31:17 +0800

    调整box64路径

--- wine-run.sh
+++ wine-run.sh
@@ -3,7 +3,7 @@ echo Power by Wine Runner
 echo 由 Wine 运行器提供支持
 CURRENT_DIR=$(cd $(dirname $0); pwd)
 cp -r $CURRENT_DIR/box64/box64.box64rc ~/.box64rc
-export LD_LIBRARY_PATH=$CURRENT_DIR/box64/x86_64-linux-gnu/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$CURRENT_DIR/box64/box64-x86_64-linux-gnu/:$LD_LIBRARY_PATH
 if [[ `uname -m` == "loongarch64" ]]; then
     if [[ `getconf PAGESIZE` != "4096" ]]; then
         echo 不支持当前内核，Pagesize 应该为 4096 而非 `getconf PAGESIZE`
