00d57e50a5d7d7b869d73d32531c60548136c4de
gfdgd_xi <3025613752@qq.com>
2026-07-31 11:36:07 +0800

    文件名改debian11
    Signed-off-by: gfdgd_xi <3025613752@qq.com>

--- build-box86.sh
+++ build-box86.sh
@@ -10,33 +10,33 @@ gpg --import  --pinentry-mode loopback --batch --passphrase "$KEYPASSWORD"  priv
 mkdir /tmp/result
 sudo aria2c -d /usr/bin -o pardus-chroot https://github.com/gfdgd-xi/deep-wine-runner/raw/main/pardus-chroot
 sudo chmod 777 /usr/bin/pardus-chroot
-sudo debootstrap --arch=amd64 buster debian10-amd64
-sudo debootstrap --arch=armhf buster debian10-armhf
-sudo debootstrap --arch=ppc64el buster debian10-ppc64el
+sudo debootstrap --arch=amd64 buster debian11-amd64
+sudo debootstrap --arch=armhf buster debian11-armhf
+sudo debootstrap --arch=ppc64el buster debian11-ppc64el
 sudo debootstrap --arch=riscv64 focal ubuntu20.04-riscv64
-sudo pardus-chroot debian10-amd64
-sudo pardus-chroot debian10-armhf
-sudo pardus-chroot debian10-arm64
-sudo pardus-chroot debian10-ppc64el
+sudo pardus-chroot debian11-amd64
+sudo pardus-chroot debian11-armhf
+sudo pardus-chroot debian11-arm64
+sudo pardus-chroot debian11-ppc64el
 sudo pardus-chroot ubuntu20.04-riscv64
-sudo aria2c -d debian10-ppc64el -o build.sh https://github.com/gfdgd-xi/auto-building-wine/raw/main/build-box86-in-chroot.sh
-sudo aria2c -d debian10-amd64 -o build.sh https://github.com/gfdgd-xi/auto-building-wine/raw/main/build-box86-in-chroot.sh
-sudo aria2c -d debian10-armhf -o build.sh https://github.com/gfdgd-xi/auto-building-wine/raw/main/build-box86-in-chroot.sh
-sudo aria2c -d debian10-arm64 -o build.sh https://github.com/gfdgd-xi/auto-building-wine/raw/main/build-box86-in-chroot.sh
+sudo aria2c -d debian11-ppc64el -o build.sh https://github.com/gfdgd-xi/auto-building-wine/raw/main/build-box86-in-chroot.sh
+sudo aria2c -d debian11-amd64 -o build.sh https://github.com/gfdgd-xi/auto-building-wine/raw/main/build-box86-in-chroot.sh
+sudo aria2c -d debian11-armhf -o build.sh https://github.com/gfdgd-xi/auto-building-wine/raw/main/build-box86-in-chroot.sh
+sudo aria2c -d debian11-arm64 -o build.sh https://github.com/gfdgd-xi/auto-building-wine/raw/main/build-box86-in-chroot.sh
 sudo aria2c -d ubuntu20.04-riscv64 -o build.sh https://github.com/gfdgd-xi/auto-building-wine/raw/main/build-box86-in-chroot.sh
-sudo chroot debian10-amd64 bash /build.sh $box64url
-sudo chroot debian10-armhf bash /build.sh $box86url
-sudo chroot debian10-arm64 bash /build.sh $box64url
-sudo chroot debian10-ppc64el bash /build.sh $box64url
+sudo chroot debian11-amd64 bash /build.sh $box64url
+sudo chroot debian11-armhf bash /build.sh $box86url
+sudo chroot debian11-arm64 bash /build.sh $box64url
+sudo chroot debian11-ppc64el bash /build.sh $box64url
 sudo chroot ubuntu20.04-riscv64 bash /build.sh $box64url
-cp debian10-armhf/box/*.deb /tmp/result/box86_${box86version}_armhf.deb -rv
-cp debian10-armhf/box/*.deb /tmp/result/box86_armhf.deb -rv
-cp debian10-amd64/box/*.deb /tmp/result/box64_${box64version}_amd64.deb -rv
-cp debian10-amd64/box/*.deb /tmp/result/box64_amd64.deb -rv
-cp debian10-arm64/box/*.deb /tmp/result/box64_${box64version}_arm64.deb -rv
-cp debian10-arm64/box/*.deb /tmp/result/box64_arm64.deb -rv
-cp debian10-ppc64el/box/*.deb /tmp/result/box64_${box64version}_ppc64el.deb -rv
-cp debian10-ppc64el/box/*.deb /tmp/result/box64_ppc64el.deb -rv
+cp debian11-armhf/box/*.deb /tmp/result/box86_${box86version}_armhf.deb -rv
+cp debian11-armhf/box/*.deb /tmp/result/box86_armhf.deb -rv
+cp debian11-amd64/box/*.deb /tmp/result/box64_${box64version}_amd64.deb -rv
+cp debian11-amd64/box/*.deb /tmp/result/box64_amd64.deb -rv
+cp debian11-arm64/box/*.deb /tmp/result/box64_${box64version}_arm64.deb -rv
+cp debian11-arm64/box/*.deb /tmp/result/box64_arm64.deb -rv
+cp debian11-ppc64el/box/*.deb /tmp/result/box64_${box64version}_ppc64el.deb -rv
+cp debian11-ppc64el/box/*.deb /tmp/result/box64_ppc64el.deb -rv
 cp ubuntu20.04-riscv64/box/*.deb /tmp/result/box64_${box64version}_riscv64.deb -rv
 cp ubuntu20.04-riscv64/box/*.deb /tmp/result/box64_riscv64.deb -rv
 cd /tmp/result
